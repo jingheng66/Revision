02b14ea83a746fa4f90e611982c5c8e6f8507ea0
jingheng66 <jingheng.tan66@gmail.com>
2024-05-19 21:04:18 +0800

    add .gitignore file

---
+++ Test_bmi.py
@@ -0,0 +1,11 @@
+import RevSubmodule2.bmi as bmi
+
+def test_bmi_normal_weight():
+    assert bmi.calculate_bmi(1.73, 70) == 0
+
+def test_bmi_over_weight():
+    assert bmi.calculate_bmi(1.73, 80) == 1
+
+def test_bmi_under_weight():
+    assert bmi.calculate_bmi(1.73, 50) == -1
+
